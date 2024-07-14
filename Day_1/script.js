be393397fa7820debdc40aa6c5fa8261451e5e3c
chandra sekhar <sekhsrchandra86@gmail.com>
2024-07-14 21:11:13 +0530

    first commit

---
+++ Day_1/script.js
@@ -0,0 +1,48 @@
+// Variable and datatype
+
+// Task 1: Declar a veriable using var ,assign it a number ,  log the value  to the console.
+
+var number = 32;
+console.log(number);
+
+// Task 2: Declar a veriable using let ,assign it a string ,log the value  to the console.
+
+let string = "30,32,46";
+console.log(string);
+
+// Task 3: Declar a veriable using const ,assign it a boolean value ,log the value  to the console.
+
+const value = true;
+console.log(value);
+
+// Task 4: creat a veriable of different data type (num,string,Boolean,obj,Array) and log each variable type using the typeof  operator.
+
+let num = 43;
+console.log(typeof num);
+
+let stringvalue = "hello! world";
+console.log(typeof stringvalue);
+
+let boolean = false;
+console.log(typeof boolean);
+
+let Obj = {
+  name: "chandra",
+  rool: "43",
+};
+console.log(typeof Obj);
+
+const arrayvalue = ["heello", 43, true];
+console.log(typeof arrayvalue);
+
+// Task 5: Declar a veriable using let ,assign it an initial value ,reassign a new value and log both value to the console.
+
+let name = "chandra";
+name = "jena";
+console.log(name);
+
+//task 6: Try reassign a veriable declared with const and obserbed the error.
+
+const flower = "rose";
+flower = "gendu";
+console.log(flower);
---
+++ day_2/script.js
@@ -0,0 +1,60 @@
+// OPERATOR
+
+// Task 1: write a pragram to add two number and log the result to the console.
+
+let num1 = 23;
+let num2 = 43;
+console.log(num1 + num2);
+
+// Task 2: write a pragram to subtract two number and log the result to the console.
+
+let value1 = 43;
+let value2 = 23;
+console.log(value1 - value2);
+
+// Task 3: write a pragram to multiply two number and log the result to the console.
+
+const multi1 = 3;
+const multi2 = 2;
+console.log(multi1 * multi2);
+
+// Task 4: write a pragram to divide two number and log the result to the console.
+
+let div1 = 12;
+let div2 = 2;
+console.log(div1 / div2);
+
+//Task 5: write a program to find the remainder when one number is divided by another and log the result to the console.
+
+let devider1 = 34;
+let devider2 = 12;
+console.log((devider1 / devider2) % 100);
+
+//Task 6: Use the += operator to add a number to a variable and log the result to the console.
+
+let add = 10;
+add += 20;
+console.log(add);
+
+//Task 7: Use the -= operator to subtract a number to a variable and log the result to the console.
+
+let substract = 25;
+substract -= 10;
+console.log(substract);
+
+//Task 8: write a program two number using > and < and log the result in console.
+
+let check = 23;
+let result = 32;
+console.log(check < result);
+
+//Task 9: write a program two number using >= and <= and log the result in console.
+
+let lessthen = 23;
+let lessthen1 = 12;
+
+if (lessthen >= lessthen1) {
+  console.log("the ans correct");
+} else {
+  console.log("the ans wrong");
+}
